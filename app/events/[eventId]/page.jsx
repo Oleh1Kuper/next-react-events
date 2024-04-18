66e6f606a24e379ea5f9afa0cd7f5bfcf4c26191
Oleh Kuper <kuper.oleh.dev@gmail.com>
2024-04-18 19:40:02 +0300

    create routes

---
+++ app/events/[eventId]/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const EventDetailPage = () => {
+  return (
+    <div>
+      <h1>Evant by Id</h1>
+    </div>
+  );
+};
+
+export default EventDetailPage;
---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+import styles from './page.module.css';
+
+export default function Home() {
+  const x = 10;
+
+  return (
+    <div>
+      <h1>Home Page</h1>
+    </div>
+  );
+}
